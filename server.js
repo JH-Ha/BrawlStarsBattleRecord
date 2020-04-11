c966c7bc236def409e95b02b245df53733796b02
JH.Ha <cubeprince@gmail.com>
2020-04-11 02:07:46 +0000

    reformat code and implement solo mode handling function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const reqModule = require("request");
 const fs = require("fs");
 const express = require("express");
 const app = express();
-const firebase = require('firebase');
+const firebase = require("firebase");
 
 let rawData = fs.readFileSync("token.json");
 let token = JSON.parse(rawData);
@@ -20,41 +20,45 @@ if (!firebase.apps.length) {
 
 const firestore = firebase.firestore();
 
-//mode 
-var GEM_GRAB = 'gemGrab';
-var BRAWL_BALL = 'brawlBall';
-var SOLO_SHOWDOWN = 'soloShowdown';
-var DUO_SHOWDOWN = 'duoShowdown';
-var TAKEDOWN = 'takedown';
-var HEIST = 'heist';
-var SIEGE = 'siege';
-var BIG_GAME = 'bigGame';
-var BOUNTY = 'bounty';
-var LONE_STAR = 'loneStar';
+//mode
+var GEM_GRAB = "gemGrab";
+var BRAWL_BALL = "brawlBall";
+var SOLO_SHOWDOWN = "soloShowdown";
+var DUO_SHOWDOWN = "duoShowdown";
+var TAKEDOWN = "takedown";
+var HEIST = "heist";
+var SIEGE = "siege";
+var BIG_GAME = "bigGame";
+var BOUNTY = "bounty";
+var LONE_STAR = "loneStar";
 
 //type
-var RANKED = 'ranked';
-var FRIENDLY = 'friendly';
+var RANKED = "ranked";
+var FRIENDLY = "friendly";
 
 //trophyChangeArray for checking it is power play or not
 var soloPointArr = [38, 34, 30, 26, 22, 18, 14, 10, 6, 2];
 var duoPointArr = [34, 26, 18, 10, 2];
 var trioPointArr = {
-    victory: 30,
-    draw: 10,
-    defeat: 5
+  victory: 30,
+  draw: 10,
+  defeat: 5,
 };
 
-
-function isTrio(mode){
-  if(mode === GEM_GRAB || mode === BRAWL_BALL || mode === HEIST 
-    || mode === BOUNTY || mode === SIEGE ){
+function isTrio(mode) {
+  if (
+    mode === GEM_GRAB ||
+    mode === BRAWL_BALL ||
+    mode === HEIST ||
+    mode === BOUNTY ||
+    mode === SIEGE
+  ) {
     return true;
   }
   return false;
 }
-function isSolo(mode){
-  if(mode === SOLO_SHOWDOWN || mode === TAKEDOWN || mode === LONE_STAR){
+function isSolo(mode) {
+  if (mode === SOLO_SHOWDOWN || mode === TAKEDOWN || mode === LONE_STAR) {
     return true;
   }
   return false;
@@ -65,7 +69,6 @@ function isDuo(mode) {
   }
   return false;
 }
-
 
 app.get("/", (req, res) => {
   let tag = req.query.tag;
@@ -91,55 +94,93 @@ app.get("/", (req, res) => {
     //res.send(body);
     //console.log(battleLogJson);
     //send(body);
-/*     reqModule.post(slackOptions, function (err, httpResponse, slackBody) { */
-      // console.log(slackBody);
+    /*     reqModule.post(slackOptions, function (err, httpResponse, slackBody) { */
+    // console.log(slackBody);
     /* }); */
     if (battleLogJson.items == undefined) {
       res.send(`${body}`);
       return;
     }
     let items = battleLogJson.items;
-    for(let i = 0; i < items.length ; i ++){
+    for (let i = 0; i < items.length; i++) {
       console.log(items[i]);
       let item = items[i];
       let mode = item.event.mode;
 
-      if(isTrio(mode)){
+      if (isTrio(mode)) {
         handleTrio(item, tag);
-      } else if(isDuo(mode)){
+      } else if (isDuo(mode)) {
         handleDuo(item, tag);
-      } else if(isSolo(mode)){
+      } else if (isSolo(mode)) {
         handleSolo(item, tag);
-      } else{
+      } else {
         console.log(`${mode} is not handled.`);
       }
-
     }
     res.send("battle logs are successfully updated");
   });
   //  res.send("express server");
 });
 
-app.get("/test", (req, res) =>{
+app.get("/test", (req, res) => {
   res.send("test success");
 });
 
 function makeDateTimeFormat(time) {
-    var year = time.substring(0, 4);
-    var month = time.substring(4, 6);
-    var date = time.substring(6, 8);
-    var hour = time.substring(9, 11);
-    var minute = time.substring(11, 13);
-    var second = time.substring(13, 15);
-    var dateTimeFormatStr =
-        year + '-' + month + '-' + date + ' ' + hour + ':' + minute + ':' + second;
-    //console.log(dateTimeFormatStr);
-    return dateTimeFormatStr;
+  var year = time.substring(0, 4);
+  var month = time.substring(4, 6);
+  var date = time.substring(6, 8);
+  var hour = time.substring(9, 11);
+  var minute = time.substring(11, 13);
+  var second = time.substring(13, 15);
+  var dateTimeFormatStr =
+    year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second;
+  //console.log(dateTimeFormatStr);
+  return dateTimeFormatStr;
 }
 
+function handleSolo(item, tag) {
+  console.log("handleSolo");
+  let battleTime = item.battleTime;
+  let battle = item.battle;
+  let mode = battle.mode;
+  let type = battle.type;
+  let rank = battle.rank;
+  let trophyChange = battle.trophyChange || 0;
+  let players = battle.players;
 
-function handleSolo(item, tag){
-  console.log('handleSolo');
+  let brawler_name = null;
+  let power = null;
+  let trophies = null;
+  for (let i = 0; i < players.length; i++) {
+    let player = players[i];
+    if (player.tag === tag) {
+      power = player.brawler.power;
+      trophies = player.brawler.trophies;
+    }
+  }
+
+  let isPowerPlay = false;
+
+  if (soloPointArr[rank - 1] === trophyChange) {
+    isPowerPlay = true;
+  }
+
+  let userCollection = firestore.collection("battleLog");
+
+  let gameInfo = {
+    battleTime: battleTimeFormat(battleTime),
+    mode: mode,
+    map: item.event.map,
+    type: type,
+    rank: rank,
+    trophyChange: trophyChange,
+    brawler_name: brawler_name,
+  };
+  if (type === RANKED) {
+    gameInfo.power = power;
+    gameInfo.trophies = trophies;
+  }
 }
 function handleDuo(item, tag) {
   console.log("handle Duo");
@@ -158,7 +199,7 @@ function handleDuo(item, tag) {
   let battleTime = item.battleTime;
   let battleTimeFormat = makeDateTimeFormat(battleTime);
 
-  let userCollection = firestore.collection(tag);
+  let userCollection = firestore.collection("battleLog");
   let isPowerPlay = false;
 
   if (trophyChange === duoPointArr[rank - 1]) {
@@ -178,6 +219,7 @@ function handleDuo(item, tag) {
   }
 
   let gameInfo = {
+    tag: tag,
     battleTime: battleTimeFormat,
     map: item.event.map,
     mode: mode,
@@ -186,15 +228,15 @@ function handleDuo(item, tag) {
     isPowerPlay: isPowerPlay,
     brawler_name: brawler_name,
     //power: power,
-//    trophies: trophies,
+    //    trophies: trophies,
   };
-  if(type === RANKED){
+  if (type === RANKED) {
     gameInfo.power = power;
     gameInfo.trophies = trophies;
   }
-  userCollection.doc(battleTime).set(gameInfo);
+  userCollection.doc(tag + "_" + battleTime).set(gameInfo);
 }
-function handleTrio(item, tag){
+function handleTrio(item, tag) {
   let battle = item.battle;
   let result = battle.result;
   let duration = battle.duration;
@@ -210,16 +252,14 @@ function handleTrio(item, tag){
 
   let mode = item.event.mode;
   let map = item.event.map;
-  
+
   let battleTime = item.battleTime;
 
-  let userCollection = firestore.collection(tag);
+  let userCollection = firestore.collection("battleLog");
 
   let battleTimeFormating = makeDateTimeFormat(battleTime);
-  
+
   //let idList = firestore.collection('ID_LIST');
-
-
 
   for (let i_t = 0; i_t < teams.length; i_t++) {
     let team = teams[i_t];
@@ -232,19 +272,18 @@ function handleTrio(item, tag){
       }
 
       /* idList */
-        // .doc(member.tag)
-        // .get()
-        // .then((doc) => {
-        //   if (!doc.exists) {
-        //     idList.doc(member.tag).set({
-        //       name: member.name,
-        //     });
-        //   }
-        // })
-        // .catch((err) => {
-        //   console.log("Error getting document", err);
-        /* }); */
-
+      // .doc(member.tag)
+      // .get()
+      // .then((doc) => {
+      //   if (!doc.exists) {
+      //     idList.doc(member.tag).set({
+      //       name: member.name,
+      //     });
+      //   }
+      // })
+      // .catch((err) => {
+      //   console.log("Error getting document", err);
+      /* }); */
     }
   }
 
@@ -255,16 +294,16 @@ function handleTrio(item, tag){
   ) {
     isPowerPlay = true;
   }
-  //check starPlayer 
+  //check starPlayer
   let isStarPlayer = false;
-  
+
   let starPlayer = battle.starPlayer;
-  if(starPlayer.tag === tag){
+  if (starPlayer.tag === tag) {
     isStarPlayer = true;
   }
 
   let isDebug = false;
-  if(isDebug){
+  if (isDebug) {
     console.log(`battleTimeFormat : ${battleTimeFormating}`);
     console.log(`map : ${map}`);
     console.log(`mode : ${mode}`);
@@ -279,7 +318,8 @@ function handleTrio(item, tag){
   }
 
   let gameInfo = {
-    type : type,
+    tag: tag,
+    type: type,
     battleTime: battleTimeFormating,
     map: map,
     mode: mode,
@@ -287,14 +327,14 @@ function handleTrio(item, tag){
     duration: duration,
     trophyChange: trophyChange,
     isPowerPlay: isPowerPlay,
-    isStarPlayer : isStarPlayer,
+    isStarPlayer: isStarPlayer,
     brawler_name: brawler_name,
-  }
-  if(type === RANKED){
+  };
+  if (type === RANKED) {
     gameInfo.power = power;
     gameInfo.trophies = trophies;
   }
-  userCollection.doc(battleTime).set(gameInfo);
+  userCollection.doc(tag + "_" + battleTime).set(gameInfo);
 }
 
 let port = 443;
@@ -303,53 +343,111 @@ app.listen(port, () => {
   console.log(`http server port on ${port}`);
 });
 
-
 //Periodically request battle logs and update them to database
 
 let options = {
-  uri : `http://localhost:${port}/`,
-  qs :{
-    tag : ''
-  }
+  uri: `http://localhost:${port}/`,
+  qs: {
+    tag: "",
+  },
 };
 
-let callUpdateAPI = function(){
-    console.log(new Date());
+let callUpdateAPI = function () {
+  console.log(new Date());
 
-    let idArr = [];
-    let testRequest = (idx)=>{
-      console.log(`idx ${idx}`);
-      if(idx >= idArr.length) return;
+  let idArr = [];
+  let testRequest = (idx) => {
+    console.log(`idx ${idx}`);
+    if (idx >= idArr.length) return;
 
-      options.qs.tag = idArr[idx];
-      console.log(options);
-      reqModule(options, function (err, response, body) {
-        console.log(body);
-        testRequest(idx +1);
+    options.qs.tag = idArr[idx];
+    console.log(options);
+    reqModule(options, function (err, response, body) {
+      console.log(body);
+      testRequest(idx + 1);
     });
-    };
+  };
 
-    let idList = firestore.collection('ID_LIST');
-    idList
-      .get()
-      .then((snapshot) => {
-        if (snapshot.empty) {
-          console.log("No matching documents.");
-          return;
-        }
-        snapshot.forEach((doc) => {
-          //console.log(doc.id);
-          idArr.push(doc.id);
-        });
-        testRequest(0);
-        //console.log(snapshot);
-        console.log(idArr);
-      })
-      .catch((err) => {
-        console.log("Error getting documents", err);
+  let idList = firestore.collection("ID_LIST");
+  idList
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        console.log("No matching documents.");
+        return;
+      }
+      snapshot.forEach((doc) => {
+        //console.log(doc.id);
+        idArr.push(doc.id);
+      });
+      testRequest(0);
+      //console.log(snapshot);
+      console.log(idArr);
+    })
+    .catch((err) => {
+      console.log("Error getting documents", err);
+    });
+};
+
+/* firestore.collection("battleLog").where("battleTime","==", "2020-04-08 18:37:19") */
+// .get()
+// .then(function(querySnapshot){
+//   querySnapshot.forEach(function(doc){
+//     console.log(doc.id, "=>", doc.data());
+//   });
+// })
+// .catch(function(error){
+//   console.log("error getting documents:", error);
+/* }); */
+
+//callUpdateAPI();
+//setInterval( callUpdateAPI , 3600 * 1000);
+//
+//
+//
+
+//remove collection from firestore
+
+function deleteCollection(db, collectionPath, batchSize) {
+  let collectionRef = db.collection(collectionPath);
+  let query = collectionRef.orderBy("__name__").limit(batchSize);
+
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(db, query, batchSize, resolve, reject);
+  });
+}
+function deleteQueryBatch(db, query, batchSize, resolve, reject) {
+  query
+    .get()
+    .then((snapshot) => {
+      // When there are no documents left, we are done
+      if (snapshot.size == 0) {
+        return 0;
+      }
+
+      // Delete documents in a batch
+      let batch = db.batch();
+      snapshot.docs.forEach((doc) => {
+        batch.delete(doc.ref);
       });
 
-  }
+      return batch.commit().then(() => {
+        return snapshot.size;
+      });
+    })
+    .then((numDeleted) => {
+      if (numDeleted === 0) {
+        resolve();
+        return;
+      }
 
-callUpdateAPI();
-setInterval( callUpdateAPI , 3600 * 1000);
+      // Recurse on the next process tick, to avoid
+      // exploding the stack.
+      process.nextTick(() => {
+        deleteQueryBatch(db, query, batchSize, resolve, reject);
+      });
+    })
+    .catch(reject);
+}
+
+
