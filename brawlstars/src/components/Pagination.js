9f1fd33abde046412481b0ddc007f8a0075bc380
Jaehyeong Ha <cubeprince@gmail.com>
2020-06-07 14:43:34 +0000

    add pagination at view userList page and make pakage.json for server code

---
+++ brawlstars/src/components/Pagination.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {BrowserRouter as Router,  Route, Link } from 'react-router-dom';
+import UserList from './UserList';
+import styles from './Pagination.scss';
+class Page{
+    constructor(link, pageNum , content){
+        this.link = link;
+        this.pageNum = pageNum;
+        this.content = content;
+    }
+}
+class Pagination extends Component{
+    
+    testOnClick = () =>{
+        console.log("click");
+    }
+    render(){
+        let {curPage, numTotal, numShowItems, pageUrl, onClick} = this.props;
+        console.log(`pagination props ${curPage}`);
+        const numShowPages = 10;
+        let maxPage = Math.floor((numTotal -1 )/ numShowItems) + 1
+        console.log(`maxPage ${maxPage}`);
+        
+        if(curPage === undefined) curPage = 1;
+        if(numShowItems === undefined) numShowItems = 15;
+        
+        if(curPage < 1) curPage = 1;
+        else if(curPage > maxPage) curPage = maxPage;
+        
+        let startPage = Math.floor((curPage - 1) / numShowPages) 
+                        * numShowPages + 1;
+        let endPage = startPage + numShowPages - 1;
+        if(endPage > maxPage) endPage = maxPage;
+        
+        let pageList = [];
+        pageList.push(new Page("", curPage -1, "<"));
+        for(let i = startPage ; i <= endPage ; i++){
+            pageList.push(new Page(`${pageUrl}?curPage=${i}`, i, i));
+        }
+        pageList.push(new Page("", curPage + 1, ">"));
+
+        console.log("chagnePageHandler", this.props.onClick);
+        return (
+            <div className="center">
+                <div className="btn-container">
+                    {pageList.map((page, index) =>{
+                        return (
+                                <button key={page.content} 
+                                className={"btn " 
+                                + (curPage == page.pageNum ? 'activate': '')
+                                }
+                                onClick = {() =>onClick(page.pageNum)}>{page.content}</button>
+                        )
+                    })}
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+export default Pagination;
--- brawlstars/src/components/UserList.js
+++ brawlstars/src/components/UserList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import firestore from './Firestore';
+import Pagination from './Pagination';
+import qs from 'qs';
 
 class User {
     constructor(tag, name) {
@@ -8,65 +10,126 @@ class User {
     }
 }
 
-const getUserList = () => {
-    return firestore.collection("ID_LIST")
-        .get().then((snapshot) => {
-            var rows = [];
-            snapshot.forEach((doc) => {
-                var data = doc.data();
-                console.log(data);
+// const getUserList = () => {
+//     return firestore.collection("ID_LIST")
+//         .get().then((snapshot) => {
+//             var rows = [];
+//             snapshot.forEach((doc) => {
+//                 var data = doc.data();
+//                 console.log(data);
 
-            })
-        });
-}
+//             })
+//         });
+// }
 
 const tableStyle = {
-    margin : "auto"
+    margin: "auto"
 }
 class UserList extends Component {
     state = {
-        userList: []
+        userList: [],
+        curPage : 0,
+        maxPage : 92,
+        pageUrl : "userList"
     }
-    
+    getQuery(){
+        const query = qs.parse(this.props.location.search, {
+            ignoreQueryPrefix : true
+        });
+        return query;
+    }
+    setUserList(snapshot){
+        var rows = [];
+        snapshot.forEach((doc) => {
+            var data = doc.data();
+            data["tag"] = doc.id;
+            console.log(data);
+            rows.push(data);
+
+        });
+        console.log(rows);
+        this.setState({ userList: rows });
+    }
+    getUserList(page){
+        console.log("getUserList");
+        this.setState({curPage : page});
+        
+        var limitNum = (page - 1) * 15;
+        if(limitNum <= 0 ) limitNum = 15;
+        var first = firestore.collection("ID_LIST").orderBy("tag")
+        .limit(limitNum);
+        let paginate = first.get()
+        .then((snapshot) =>{
+            if(page == 1) this.setUserList(snapshot);
+            else{
+                console.log("else");
+                let last = snapshot.docs[snapshot.docs.length - 1];
+                let next = firestore.collection("ID_LIST").orderBy("tag")
+                .startAfter(last.data().tag)
+                .limit(15);
+                next.get().then((snapshot)=>{
+                    this.setUserList(snapshot);
+                });
+            }
+        });
+    }
     componentDidMount() {
         console.log("mount");
-        firestore.collection("ID_LIST")
-            .get().then((snapshot) => {
-                var rows = [];
-                snapshot.forEach((doc) => {
-                    var data = doc.data();
-                    data["tag"] = doc.id;
-                    console.log(data);
-                    rows.push(data);
-                    
-                });
-                console.log(rows);
-                this.setState({userList : this.state.userList.concat(rows)});
-                //this.setState({ userList: rows });
-            });
+        let query = this.getQuery();
+        let curPage = parseInt(query.curPage);
+        console.log(`curPage ${curPage}`);
+        if(curPage === undefined) curPage = 1;
+        this.getUserList(curPage);
+        
+           
+        // firestore.collection("ID_LIST").orderBy("tag").limit(15)
+        //     .get().then((snapshot) => {
+        //         var rows = [];
+        //         snapshot.forEach((doc) => {
+        //             var data = doc.data();
+        //             data["tag"] = doc.id;
+        //             console.log(data);
+        //             rows.push(data);
+
+        //         });
+        //         console.log(rows);
+        //         this.setState({ userList: this.state.userList.concat(rows) });
+        //         //this.setState({ userList: rows });
+        //     });
+    }
+    changePageHandler(page){
+        console.log("changePageHandler");
+            this.setState({curPage : page});
+            this.getUserList(page);
+        
     }
 
     render() {
         return (
+            
             <div>
                 <h1>UserList</h1>
                 <table style={tableStyle}>
                     <thead>
 
-                        <th><td>순번</td></th>
-                        <th><td>닉네임(태그)</td></th>
+                        <tr><th>Index</th>
+                            <th>Nickname(tag)</th></tr>
                     </thead>
                     <tbody>
-                {this.state.userList.map((user, index) => {
-                    return(
-                        <tr>
-                            <td>{index + 1}</td>
-                    <td key={user.tag}>
-                {user.name}({user.tag})</td>
-                </tr>
-                )})}
-                </tbody>
+                        {this.state.userList.map((user, index) => {
+                            return (
+                                <tr key={user.tag} page={this.state.curPage}>
+                                    <td>{(this.state.curPage - 1) * 15 + index + 1}</td>
+                                    <td>
+                                        {user.name}({user.tag})</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
                 </table>
+                <Pagination curPage={this.state.curPage} numTotal = "92" 
+                numShowItems = "15" pageUrl="/userList"
+                onClick={this.changePageHandler.bind(this)}/>
             </div>
         )
     }
