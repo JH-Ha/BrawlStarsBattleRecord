7316bc6d164e128db718563f84ff91d488503657
Jaehyeong Ha <cubeprince@gmail.com>
2021-02-03 01:36:01 +0900

    add springboot

---
+++ springBootBrawlStars/src/test/java/com/brawlstars/SpringBootBrawlStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brawlstars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBrawlStarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
