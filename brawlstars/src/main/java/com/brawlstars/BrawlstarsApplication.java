395404c1ee430ae178d63a8334a0b890d9df3a79
Jaehyeong Ha <cubeprince@gmail.com>
2021-02-23 00:11:53 +0900

    SpringBoot setting

---
+++ brawlstars/src/main/java/com/brawlstars/BrawlstarsApplication.java
@@ -0,0 +1,13 @@
+package com.brawlstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrawlstarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrawlstarsApplication.class, args);
+	}
+
+}
