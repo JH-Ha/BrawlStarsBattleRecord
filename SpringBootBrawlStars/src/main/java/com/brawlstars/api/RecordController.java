fb5e5b08147ac26de77a0f8082346c82ddace59d
Jaehyeong Ha <cubeprince@gmail.com>
2021-02-24 01:43:48 +0900

    get battle log from api server

--- SpringBootBrawlStars/src/main/java/com/brawlstars/api/RecordController.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/api/RecordController.java
@@ -1,10 +1,16 @@
 package com.brawlstars.api;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +27,42 @@ public class RecordController {
 	@GetMapping("/record/save/{tag}")
 	public String saveRecords(@PathVariable(name = "tag") String tag) {
 		String baseUrl = "https://api.brawlstars.com/v1/players/";
+		tag = tag.replace("#", "%23");
 		try {
-			URL url = new URL(baseUrl + tag);
+			URL url = new URL(baseUrl + tag + "/battlelog");
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-			String temp;
-			// while((temp = br))
-		} catch (IOException e) {
+			String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjAzM2YwNDRmLTIzZTUtNGJhZC04ZDdhLWVlODgzOTk0NDIyMSIsImlhdCI6MTYxNDA5Mzc2NSwic3ViIjoiZGV2ZWxvcGVyLzRmODljMjEyLWE0OWYtZWRiZS0zZjgzLTRhODJiZGE2N2NiNSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNTguMTQzLjIwNi40MSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.YknOO4aOE7OxNQ1SWZ-ojAp73SqDMXxiA-hyUzHnPcF36HiOQeFPUgHMv6LmHEA8YwWiSL-tWBRRfZZAZvrA6g";
+			conn.setRequestProperty("Authorization", "Bearer " + token);
+			conn.setRequestMethod("GET");
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			String output;
+
+			StringBuffer response = new StringBuffer();
+
+			while ((output = in.readLine()) != null) {
+				response.append(output);
+			}
+			in.close();
+
+			System.out.println("Response: " + response.toString());
+
+			JSONParser parser = new JSONParser();
+			JSONObject battleLog = null;
+			battleLog = (JSONObject) parser.parse(response.toString());
+			System.out.println(battleLog);
+
+			if (battleLog != null) {
+				JSONArray items = (JSONArray) battleLog.get("items");
+				for (int i = 0; i < items.size(); i++) {
+					JSONObject item = (JSONObject) items.get(i);
+					String mode = (String) ((JSONObject) item.get("battle")).get("mode");
+					System.out.println(mode);
+				}
+			}
+
+		} catch (IOException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
