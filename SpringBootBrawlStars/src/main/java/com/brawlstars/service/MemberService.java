9e442aa84869a3fb24b808eb51a383c3a58f4125
Jaehyeong Ha <cubeprince@gmail.com>
2021-03-14 20:15:04 +0900

    add MemberDto to find Members

--- SpringBootBrawlStars/src/main/java/com/brawlstars/service/MemberService.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/service/MemberService.java
@@ -3,9 +3,12 @@ package com.brawlstars.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.brawlstars.repository.MemberDto;
 import com.brawlstars.repository.MemberRepository;
 
 @Service
@@ -14,5 +17,7 @@ public class MemberService {
 	@Autowired
 	MemberRepository memberRepository;
 	
-	
+	public Page<MemberDto> getMembers(String name, Pageable pageable){
+		return memberRepository.findAll(name,pageable);
+	}
 }
