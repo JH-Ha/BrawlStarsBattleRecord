010b7bd1893d93de7ead9781b6a731fbc66f96b6
Jaehyeong Ha <cubeprince@gmail.com>
2021-03-17 23:20:22 +0900

    add Accept feild in HTTP Connection

--- SpringBootBrawlStars/src/main/java/com/brawlstars/api/BrawlStarsAPI.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/api/BrawlStarsAPI.java
@@ -26,6 +26,7 @@ public class BrawlStarsAPI {
 			
 			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			conn.setRequestProperty("Accept", "application/json");
 			String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjgxYmYyZTVjLWVjNWUtNGY0NC1iM2NhLWQ5ZjI3MzlmYmJlOSIsImlhdCI6MTYxNTk4MTc1OCwic3ViIjoiZGV2ZWxvcGVyLzRmODljMjEyLWE0OWYtZWRiZS0zZjgzLTRhODJiZGE2N2NiNSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNTguMTQzLjIwNi40MSIsIjExMC4xMC4xNTQuNjgiLCIxNS4xNjUuMTgyLjIyOCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.7vQ8OoFdLQfFRTIkrzqeA3Q4uh732LWX82T4_jVWWJKB0Ghk_DGxyH21iOhxL4hyKMSSzNyxL9g4cyHJrkdTEg";
 			conn.setRequestProperty("Authorization", "Bearer " + token);
 			conn.setRequestMethod("GET");
