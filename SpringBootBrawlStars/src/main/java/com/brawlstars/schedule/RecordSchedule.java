7cd790f7dc48ba78d97788ff56a4a5e61970b510
Jaehyeong Ha <cubeprince@gmail.com>
2021-03-18 00:06:12 +0900

    refactoring scheduler to use API component

--- SpringBootBrawlStars/src/main/java/com/brawlstars/schedule/RecordSchedule.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/schedule/RecordSchedule.java
@@ -1,11 +1,5 @@
 package com.brawlstars.schedule;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +8,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.brawlstars.json.BattleLog;
+import com.brawlstars.api.BrawlStarsAPI;
 import com.brawlstars.json.Item;
 import com.brawlstars.repository.MemberDto;
 import com.brawlstars.repository.MemberRepository;
 import com.brawlstars.service.RecordService;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class RecordSchedule {
+	@Autowired
+	BrawlStarsAPI brawlStarsAPI;
+
 	@Autowired
 	RecordService recordService;
 
@@ -32,12 +27,12 @@ public class RecordSchedule {
 
 	// one hour
 	@Scheduled(fixedDelay = 3600000
-			//,initialDelay = 360000
-			)
+	// ,initialDelay = 360000
+	)
 	public void saveRecordsSchedule() {
 		saveRecords();
 	}
-	
+
 	public void saveRecords() {
 
 		Page<MemberDto> members = memberRepository.findAll("", PageRequest.of(0, 1000));
@@ -55,39 +50,10 @@ public class RecordSchedule {
 	}
 
 	public void saveRecord(String tag) {
-		System.out.println(new Date());
 
-		String baseUrl = "https://api.brawlstars.com/v1/players/";
-		String tagReplaced = tag.replace("#", "%23");
+		List<Item> items;
 		try {
-			URL url = new URL(baseUrl + tagReplaced + "/battlelog");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-			String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjAzM2YwNDRmLTIzZTUtNGJhZC04ZDdhLWVlODgzOTk0NDIyMSIsImlhdCI6MTYxNDA5Mzc2NSwic3ViIjoiZGV2ZWxvcGVyLzRmODljMjEyLWE0OWYtZWRiZS0zZjgzLTRhODJiZGE2N2NiNSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNTguMTQzLjIwNi40MSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.YknOO4aOE7OxNQ1SWZ-ojAp73SqDMXxiA-hyUzHnPcF36HiOQeFPUgHMv6LmHEA8YwWiSL-tWBRRfZZAZvrA6g";
-			conn.setRequestProperty("Authorization", "Bearer " + token);
-			conn.setRequestMethod("GET");
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-			String output;
-
-			StringBuffer response = new StringBuffer();
-
-			while ((output = in.readLine()) != null) {
-				response.append(output);
-			}
-			in.close();
-
-			System.out.println("Response: " + response.toString());
-
-			ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-					false);
-
-			// ObjectMapper mapper = new ObjectMapper();
-
-			BattleLog battleLog = mapper.readValue(response.toString(), BattleLog.class);
-
-			List<Item> items = battleLog.getItems();
-
+			items = brawlStarsAPI.getItems(tag);
 			items.stream().forEach(item -> {
 				System.out.println(item.getBattleTime());
 				if (recordService.isTrioMode(item.getEvent().getMode())) {
@@ -98,9 +64,10 @@ public class RecordSchedule {
 					recordService.saveSolo(tag, item);
 				}
 			});
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 }
