5ad99617bf7f5fc3f65ca9427c5ca42fa4586e53
JH-Ha <cubeprince@gmail.com>
2021-03-28 20:36:52 +0900

    update home message

--- brawlstars/src/components/Home.js
+++ brawlstars/src/components/Home.js
@@ -21,6 +21,7 @@ class Home extends Component {
         style={{
           "marginTop": "30px",
         }}
+        className="home"
       >
         <div className="languageContainer">
           {languageList.map((lang, index) => (
@@ -38,50 +39,65 @@ class Home extends Component {
         </div>
 
         {this.state.language === "korean" ? (
-          <div>
-            브롤스타즈 전적 기록 사이트입니다.
+          <div className="contentContainer">
+            <div className="content">
+              브롤스타즈 전적 기록 사이트입니다.
             <br />
+              <br />
+            User List를 클릭하여
+            자신의 닉네임을 검색하고
+            전적을 확인해보세요.
+            <br />
+            Map List에서는 맵별 브롤러 승률을 알 수 있습니다.
+            <br />
+              <br />
             1시간 마다 전적이 동기화됩니다.
             <br />
-            User List를 클릭하여 <br />
-            자신의 닉네임을 검색하고
-            <br />
-            전적을 확인해보세요
-            <br />
-            <br />
+              <br />
+
             email : cubeprince@gmail.com
+            </div>
           </div>
         ) : (
           ""
         )}
         {this.state.language === "english" ? (
-          <div>
-            This is the Brawl Stars game record site.
-            <br />
-            Game records are synchronized every hour.
-            <br />
-            Click on the User List on the top menu
-            <br />
-            to search your nickname and check the history
-            <br />
-            <br />
-            email : cubeprince@gmail.com
+          <div className="contentContainer">
+            <div className="content">
+              This is the Brawl Stars game record site.
+              <br />
+              Click on the User List on the top menu
+              <br />
+              to search your nickname and check the history
+              <br />
+              Check brawlers' win rate in Map List menu.
+              <br />
+              Game records are synchronized every hour.
+              <br />
+              <br />
+              email : cubeprince@gmail.com
+            </div>
           </div>
         ) : (
           ""
         )}
         {this.state.language === "japanese" ? (
-          <div>
-            ブロールスターズの戦闘記録サイトです。
-            <br />
-            戦闘記録は1時間ごとに同期されます。
-            <br />
-            UserListをクリックして、自分のニックネームを検索して、
-            <br />
-            戦闘記録を確認してください。
-            <br />
-            <br />
-            email : cubeprince@gmail.com
+          <div className="contentContainer">
+            <div className="content">
+              ブロールスターズの戦闘記録サイトです。
+              <br />
+              UserListをクリックして、自分のニックネームを検索して、
+              <br />
+              戦闘記録を確認してください。
+              <br />
+              MapListではマップごとのキャラクターの勝率を確認できます。
+              <br />
+              <br />
+              戦闘記録は1時間ごとに同期されます。
+              <br />
+              <br />
+              email : cubeprince@gmail.com
+            </div>
           </div>
         ) : (
           ""
