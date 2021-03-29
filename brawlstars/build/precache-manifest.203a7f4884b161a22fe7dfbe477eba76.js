a22c171c10520b5db2c86f40303c3869eb528946
JH-Ha <cubeprince@gmail.com>
2021-03-29 22:03:52 +0900

    update build directory

---
+++ brawlstars/build/precache-manifest.203a7f4884b161a22fe7dfbe477eba76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058d266c2af28ccd142f8c69148a9e90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84647fd6ebfc209efa8c",
+    "url": "/static/css/main.b7d81fb8.chunk.css"
+  },
+  {
+    "revision": "99ff86e5578e526edfa7",
+    "url": "/static/js/2.bbc0aabe.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.bbc0aabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84647fd6ebfc209efa8c",
+    "url": "/static/js/main.743cc938.chunk.js"
+  },
+  {
+    "revision": "05d0af42e80a35a3fcc2",
+    "url": "/static/js/runtime-main.132ad4af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
