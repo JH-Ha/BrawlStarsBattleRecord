e6697c9f530ca41f309d63253ea2a16a2d75e239
JH-Ha <cubeprince@gmail.com>
2021-04-02 01:26:36 +0900

    [front] update statistic page

--- brawlstars/src/components/RecordResult.js
+++ brawlstars/src/components/RecordResult.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSortUp, faSortDown, faSort } from '@fortawesome/free-solid-svg-icons'
-import { isTrio } from './BaseFunctions';
+import { isTrio, isDuo, isSolo, isAll } from './BaseFunctions';
+import style from "./RecordResult.scss";
 
 class RecordResult extends Component {
     state = {
@@ -16,59 +17,99 @@ class RecordResult extends Component {
         })
     }
     render() {
-        const { mode } = this.props;
+        const { mode, isPersonal } = this.props;
 
-        return <div className="infoContainer">
-            <table className="table info">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Name</th>
-                        {isTrio(mode) ?
-                            <th>Win Rate <FontAwesomeIcon icon={faSortDown} /></th>
-                            :
-                            <th>Avg Rank <FontAwesomeIcon icon={faSortDown} /></th>
-                        }
-                        <th>Pick Rate <FontAwesomeIcon icon={faSort} /></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.recordArr.map((ele, index) => {
-                        return (
-                            <tr key={ele.brawlerName}>
-                                <td>{index + 1}</td>
-                                <td>
-                                    {/* <div className="info" key={index}> */}
-                                    <div className="brawler">
-                                        <img src={`./images/${ele.brawlerName}.png`}></img>
-                                        <div className="brawlerName">
-                                            {ele.brawlerName}
+        return <div className="recordResult">
+            <div className="infoContainer">
+                <table className="table info">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            {isTrio(mode) ?
+                                <th>Win Rate <FontAwesomeIcon icon={faSortDown} /></th>
+                                :
+                                isDuo(mode) || isSolo(mode) ?
+                                    <th>Avg Rank <FontAwesomeIcon icon={faSortDown} /></th>
+                                    : <th># of Games</th>
+                            }
+                            <th>Pick Rate <FontAwesomeIcon icon={faSort} /></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.recordArr.map((ele, index) => {
+                            return (
+                                <tr key={ele.brawlerName}>
+                                    <td>{index + 1}</td>
+                                    <td>
+                                        {/* <div className="info" key={index}> */}
+                                        <div className="brawler">
+                                            <img src={`./images/${ele.brawlerName}.png`}></img>
+                                            <div className="brawlerName">
+                                                {ele.brawlerName}
+                                            </div>
                                         </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    {ele.winRate === undefined ?
-                                        <div className="averageRank">
-                                            {Math.round(ele.averageRank * 100) / 100}
-                                        </div>
-                                        :
-                                        <div className="winRate">
-                                            {Math.round(ele.winRate * 1000) / 10}%
-                                    </div>
-                                    }
+                                    </td>
+                                    <td>
+                                        {(isDuo(mode) || isSolo(mode)) ?
+                                            <div className="averageRank">
+                                                {Math.round(ele.averageRank * 100) / 100}
+                                            </div>
+                                            : isTrio(mode) ?
 
-                                </td>
-                                <td>
-                                    <div className="totalGame">
-                                        {Math.round(ele.totalGameNum / this.state.sumTotalGameNum * 10000) / 100}%
+                                                <div className="winRate">
+                                                    {Math.round(ele.winRate * 1000) / 10}%
+
+                                                </div>
+                                                : <div className="numberOfGames">
+                                                    {ele.totalGameNum}
+                                                </div>
+                                        }
+                                    </td>
+
+                                    <td>
+                                        <div className="totalGame">
+                                            {Math.round(ele.totalGameNum / this.state.sumTotalGameNum * 10000) / 100}%
                             </div>
-                                </td>
-                                {/* </div> */}
+                                    </td>
+                                    {/* </div> */}
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+
+                    {isPersonal ?
+                        <tfoot>
+                            {isAll(mode) ?
+                                <tr>
+                                    <td colSpan="2">Total # of Games</td>
+                                    <td colSpan="2">{this.state.sumTotalGameNum} games</td>
+                                </tr>
+                                : isTrio(mode) ?
+                                    <tr>
+                                        <td colSpan="2">Avg Win Rate</td>
+                                        <td>{Math.round(this.state.recordArr.map(e => e.victory).reduce((a, b) => a + b, 0) / this.state.sumTotalGameNum * 1000) / 10}%</td>
+                                        <td>{this.state.sumTotalGameNum} games</td>
+                                    </tr>
+                                    : (isSolo(mode) || isDuo(mode)) ?
+                                        <tr>
+                                            <td colSpan="2">Avg Rank</td>
+                                            <td>{Math.round(this.state.recordArr.map(e => e.averageRank * e.totalGameNum).reduce((a, b) => a + b, 0) / this.state.sumTotalGameNum * 10) / 10}</td>
+                                            <td>{this.state.sumTotalGameNum} games</td>
+                                        </tr>
+                                        : ""
+
+                            }
+                        </tfoot>
+                        : <tfoot>
+                            <tr>
+                                <td colSpan="2">Total # of Games</td>
+                                <td colSpan="2">{this.state.sumTotalGameNum} games</td>
                             </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+                        </tfoot>
+                    }
+                </table>
+            </div>
         </div>
     }
 }
