e37a4859fce2827d5fc0bf2f4b02751dad1e473d
JH-Ha <cubeprince@gmail.com>
2021-04-04 23:14:39 +0900

    [front] change port number

--- brawlstars/src/components/ApiHandler.js
+++ brawlstars/src/components/ApiHandler.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://brawlstat.xyz:8080";
+const baseUrl = "http://brawlstat.xyz";
 //const baseUrl = "http://localhost:8080";
 
 let getData = (paramUrl) => {
