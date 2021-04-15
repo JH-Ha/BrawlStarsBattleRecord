7122b1f5938e4ed282616c604f64fe37b9aa6c05
JH-Ha <cubeprince@gmail.com>
2021-04-16 00:40:12 +0900

    [front] showdown display update

--- brawlstars/src/components/SoloDuoMode.js
+++ brawlstars/src/components/SoloDuoMode.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styles from "./SoloDuoMode.scss";
 import { calDisplayTime } from './BaseFunctions';
+import PlayerTile from './PlayerTile';
+import { withTranslation } from 'react-i18next';
 
 class SoloDuoMode extends Component {
   state = {
@@ -39,7 +41,7 @@ class SoloDuoMode extends Component {
     //   trophyChange,
     //   groupRecords,
     // } = this.props;
-    const { groupRecords, tag, map, mode, type } = this.props;
+    const { groupRecords, tag, map, mode, type, t } = this.props;
     let battleTime = "";
     let rank = "";
     let brawlerName = "";
@@ -56,47 +58,59 @@ class SoloDuoMode extends Component {
         trophyChange = e.trophyChange;
       }
     })
+    let displayGroup = [];
+    for (let i = 0; i < 5; i++) {
+      displayGroup.push([]);
+    }
+    //rank 로 정렬
+    groupRecords.sort((a, b) => {
+      return a.resultRank - b.resultRank;
+    })
+    for (let i = 0; i < groupRecords.length; i++) {
+      const groupIdx = Math.floor(i / 2);
+      displayGroup[groupIdx].push(groupRecords[i]);
+    }
 
     return (
       <div className="center">
         <div className={`SoloDuoModeContainer rank${this.state.rankCss}`}>
-          <div className={`playerContainer`}>
-            {groupRecords.map(e => {
-              return <img width="50px" height="50px" src={`/images/${e.brawlerName}.png`} />
-            })}
-          </div>
-          <div>
-            {type}
+          <div className={`gameInfoContainer rank${this.state.rankCss}`}>
+            <div className="left"></div>
+            <div className={`type`}>
+              {type}
+            </div>
+            <div className="battleTime">{calDisplayTime(battleTime)}</div>
           </div>
           <div className={`topContainer rank${this.state.rankCss}`}>
-            <div className={`rank rank${this.state.rankCss}Content`}>
-              {rank}
+            <div className="modeInfo">
+              <div className="showdonwImgContainer">
+                <img src="/images/mode/showdown.png"></img>
+              </div>
+              <div className="modeMapContainer">
+                <div className="mode">{t(mode)}
+                </div>
+                <div className="map">{t(map)}</div>
+              </div>
             </div>
-            <div>{calDisplayTime(battleTime)}</div>
-            <div>{this.state.trophyChange}</div>
+            <div className={`rank rank${this.state.rankCss}Content`}>
+              Rank {rank}
+            </div>
+
+            <div className="trophyChange">{this.state.trophyChange}</div>
           </div>
           <div className={`gameInfoContainer rank${this.state.rankCss}`}>
-            <div>
-              <img src={`/images/${brawlerName}.png`} width="50px" />
-              <div>{brawlerName}</div>
-            </div>
-            <div className="tableContainer">
-              <table>
-                <thead>
-                  <tr>
-                    <th>map</th>
-                    <th>power</th>
-                    <th>trophies</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{map}</td>
-                    <td>{power}</td>
-                    <td>{trophies}</td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className={`playerContainer`}>
+              {displayGroup.map(g => {
+                return <div>
+                  {g.map(player => {
+                    return <PlayerTile brawlerName={player.brawlerName}
+                      playerName={player.playerName}
+                      trophies={player.trophies}
+                      power={player.power} />
+                  })}
+                </div>
+                //return <img width="50px" height="50px" src={`/images/${e.brawlerName}.png`} />
+              })}
             </div>
           </div>
         </div>
@@ -105,4 +119,4 @@ class SoloDuoMode extends Component {
   }
 }
 
-export default SoloDuoMode;
+export default withTranslation()(SoloDuoMode);
