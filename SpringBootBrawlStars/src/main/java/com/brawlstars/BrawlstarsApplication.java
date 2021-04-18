78dc49e652b3f2a0649a3394a5dd93dd266dd64e
Jaehyeong Ha <cubeprince@gmail.com>
2021-04-18 22:03:03 +0900

    [back] redirect ip access or 8080 port to domain:80

--- SpringBootBrawlStars/src/main/java/com/brawlstars/BrawlstarsApplication.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/BrawlstarsApplication.java
@@ -2,10 +2,12 @@ package com.brawlstars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ServletComponentScan
 public class BrawlstarsApplication {
 
 	public static void main(String[] args) {
