621dfed1c1b489c9ede14e597e7cc2efdecc1b57
Jaehyeong Ha <cubeprince@gmail.com>
2021-04-18 22:03:03 +0900

    [back] redirect ip access or 8080 port to domain:80

---
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/filter/UrlRewriteFilter.java
@@ -0,0 +1,31 @@
+package com.brawlstars.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = "/*")
+public class UrlRewriteFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String requestURL = req.getServerName();
+		//ip access or 8080 port access go to brawlstat.xyz:80 
+		if(!requestURL.contains("brawlstat.xyz") || req.getServerPort() == 8080) {
+			res.sendRedirect("http://brawlstat.xyz" + req.getRequestURI());
+		}else {
+			chain.doFilter(req, res);
+		}		
+	}
+
+}
