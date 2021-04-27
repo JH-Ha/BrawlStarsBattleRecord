9004fedc428357228e2c75ce839aa638cf4a95de
Jaehyeong Ha <cubeprince@gmail.com>
2021-04-28 00:19:51 +0900

    add api : save player tag

--- SpringBootBrawlStars/src/main/java/com/brawlstars/api/MemberController.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/api/MemberController.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brawlstars.json.playerInfo.PlayerInfo;
 import com.brawlstars.repository.MemberDto;
+import com.brawlstars.repository.ResultDto;
 import com.brawlstars.service.MemberService;
 
 import lombok.Getter;
@@ -33,6 +35,10 @@ public class MemberController {
 	@GetMapping("/member/{tag}")
 	public MemberDto getMember(@PathVariable(name = "tag") String tag) {
 		return memberService.getMemberByTag(tag);
+	}
+	@PostMapping("/member/{tag}")
+	public ResultDto saveMember(@PathVariable(name = "tag") String tag) {
+		return memberService.saveMember(tag);
 	}
 	
 	@GetMapping("/v1/member/api/{tag}")
---
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/repository/ResultDto.java
@@ -0,0 +1,15 @@
+package com.brawlstars.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResultDto {
+	Boolean result;
+	String msg;
+	public ResultDto(){
+		this.result = false;
+		this.msg = "";
+	}
+}
