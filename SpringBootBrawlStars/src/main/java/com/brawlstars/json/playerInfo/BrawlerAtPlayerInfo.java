d04f15fc6f352d94b4daf22060593127abc96d7e
Jaehyeong Ha <cubeprince@gmail.com>
2021-04-28 00:05:58 +0900

    add api getting player info

--- SpringBootBrawlStars/src/main/java/com/brawlstars/json/playerInfo/BrawlerAtPlayerInfo.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/json/playerInfo/BrawlerAtPlayerInfo.java
@@ -6,6 +6,13 @@ import lombok.Data;
 
 @Data
 public class BrawlerAtPlayerInfo {
+	Integer id;
+	String name;
+	Integer power;
+	Integer rank;
+	Integer trophies;
+	Integer highestTrophies;
+	
 	List<StarPower> starPowers;
 	List<Gadget> gadgets;
 }
