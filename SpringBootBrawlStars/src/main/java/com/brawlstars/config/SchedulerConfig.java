724225d3e05af4727da05419bf319d25d2011d15
Jaehyeong Ha <cubeprince@gmail.com>
2021-10-04 21:22:50 +0900

    change scheduler pool size from 1 to 4

---
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/config/SchedulerConfig.java
@@ -0,0 +1,21 @@
+package com.brawlstars.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+    private final static int POOL_SIZE = 4;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        final ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
