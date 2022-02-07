7602d317533a87a30f5a75931ba61a8cd8c017ab
JH-Ha <cubeprince@gmail.com>
2022-02-07 20:08:50 +0900

    [front] change tag div -> nav

--- brawlstars/src/components/TopBar.js
+++ brawlstars/src/components/TopBar.js
@@ -36,7 +36,7 @@ class TopBar extends Component {
   }
   render() {
     return (
-      <div className="naviBar">
+      <nav className="naviBar">
         <div className="logo">
           <Link to="/" className="container">
             <img src="/brawlMeta.png" alt="brawlMeta.png" />
@@ -44,21 +44,21 @@ class TopBar extends Component {
             </div>
           </Link>
         </div>
-        <div className={`menuContainer ${this.state.toggleActive && "active"}`}>
-          <Link to="/" className="item">Home</Link>
-          <Link to="/info" className="item">Info</Link>
-          <Link to="/userList" className="item">Players</Link>
-          <Link to="/mapList" className="item">Maps</Link>
+        <ul className={`menuContainer ${this.state.toggleActive && "active"}`}>
+          <li><Link to="/" className="item">Home</Link></li>
+          <li><Link to="/info" className="item">Info</Link></li>
+          <li><Link to="/userList" className="item">Players</Link></li>
+          <li><Link to="/mapList" className="item">Maps</Link></li>
           <div className="languageContainer">
             <div className={`language ${this.state.language === 'en' ? 'selected' : ''}`} onClick={this.changelanguageToEn}>en</div>
             <div className={`language ${this.state.language === 'ko' ? 'selected' : ''}`} onClick={this.changelanguageToKo}>ko</div>
             <div className={`language ${this.state.language === 'ja' ? 'selected' : ''}`} onClick={this.changelanguageToJa}>ja</div>
           </div>
-        </div>
+        </ul>
         <div className="toggleBtn" onClick={this.clickToggleBtn}>
           <FontAwesomeIcon icon={faBars} />
         </div>
-      </div>
+      </nav>
     );
   }
 }
