588e7523636b0a9b421e916c9819471ffcdb732a
JH-Ha <cubeprince@gmail.com>
2022-02-07 19:56:28 +0900

    [front] change tag div -> footer

--- brawlstars/src/components/Footer.js
+++ brawlstars/src/components/Footer.js
@@ -5,11 +5,11 @@ import "./Footer.scss";
 const Footer = () => {
 
     return (
-        <div className='footer'>
+        <footer className='footer'>
             This material is unofficial and is not endorsed by Supercell.
             <br />
             For more information see <a href="https://www.supercell.com/fan-content-policy">Supercell's Fan Content Policy</a>
-        </div>
+        </footer>
     )
 };
 
