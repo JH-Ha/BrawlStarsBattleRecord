adaf1dafe8afe3a57af1c0614e983a4c908aa60e
JH-Ha <cubeprince@gmail.com>
2022-02-20 20:16:13 +0900

    [front] navbar and footer migration

--- nextjs-brawlstars/next.config.js
+++ nextjs-brawlstars/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+// module.exports = nextConfig
+
+module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'ko', 'ja'],
+    defaultLocale: 'en',
+  },
 }
 
-module.exports = nextConfig
+
