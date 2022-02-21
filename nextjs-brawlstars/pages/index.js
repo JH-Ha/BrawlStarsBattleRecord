fe17a39830685416242566a18797a75834db5f00
JH-Ha <cubeprince@gmail.com>
2022-02-22 02:08:37 +0900

    [front] eventRotation server side rendering

--- nextjs-brawlstars/pages/index.js
+++ nextjs-brawlstars/pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import EventRotation from '../components/eventRotation'
-export default function Home() {
+import { getData } from '../components/ApiHandler'
+import { calWinRate, getLocalTime, isTrio } from '../components/BaseFunctions';
+
+export default function Home({ todayEvents, nextEvents }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +14,47 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <EventRotation />
+      <EventRotation todayEvents={todayEvents} nextEvents={nextEvents} />
     </div>
   )
+}
+
+
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  // Pass data to the page via props
+
+  const res = await getData("/api/events/rotation");
+  //const data = await res.json();
+
+  const events = res.data;
+  let now = new Date();
+  let todayEvents = [];
+  let nextEvents = [];
+
+  //console.log(showdownEvents);
+
+  events
+    .filter(e => e.event.mode !== 'duoShowdown')
+    .forEach(e => {
+      const startTime = getLocalTime(e.startTime);
+      const endTime = getLocalTime(e.endTime);
+      if ((startTime < now) && (now <= endTime)) {
+        todayEvents.push(e);
+      } else {
+        nextEvents.push(e);
+      }
+      //console.log(`${startTime} - ${now} -${endTime}`);
+      if (e.event.mode === 'soloShowdown' && e.startTime.substr(9, 2) ===
+        "08") {
+        e.event.isPlus = true;
+      }
+      if (e.statistics !== null) {
+        e.winRate = calWinRate(e.statistics, e.event.mode).slice(0, 5);
+      }
+    });
+
+
+  return { props: { todayEvents, nextEvents } }
 }
