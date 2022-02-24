e275301b8dccf0fc06147a76c1278c4b885cac85
JH-Ha <cubeprince@gmail.com>
2022-02-25 00:02:52 +0900

    [front] bug fix userList

--- nextjs-brawlstars/pages/userList.js
+++ nextjs-brawlstars/pages/userList.js
@@ -22,36 +22,27 @@ async function getUserList(page, nickname) {
 
 }
 
-export default function UserList(userList, curPage, numUser, propNickname) {
+export default function UserList({ userList, curPage, numUser, propNickname }) {
   const router = useRouter();
   const { t } = useTranslation();
 
-  // state = {
-  //   userList: [],
-  //   curPage: 0,
-  //   numUser: 0,
-  //   pageUrl: "userList",
-  //   nickname: "",
-  // };
   const [nickname, setNickname] = useState(propNickname);
 
 
   const changePageHandler = (page) => {
-    router.push(encodeURIComponent(`/userList?curPage=${page}&nickname=${nickname}`));
+    router.push({
+      pathname: "/userList",
+      query: {
+        page: page,
+        nickname: encodeURIComponent(nickname),
+      }
+    });
   }
   const showPlayList = (tag) => {
     tag = tag.replace("#", "%23");
     router.push(`/battleLog/${tag}`);
   }
-  const searchNickname = () => {
-    router.push({
-      pathname: "/userList",
-      query: {
-        page: 0,
-        nickname: encodeURIComponent(nickname),
-      }
-    });
-  }
+
   const searchInputChange = (event) => {
     let value = event.target.value;
     setNickname(value);
@@ -74,7 +65,7 @@ export default function UserList(userList, curPage, numUser, propNickname) {
           onChange={searchInputChange}
           value={nickname}
         ></input>
-        <button onClick={searchNickname} className="btn btn-primary">
+        <button onClick={() => changePageHandler(0)} className="btn btn-primary">
           search
         </button>
       </div>
@@ -120,14 +111,15 @@ export async function getServerSideProps(context) {
   let { nickname, page } = context.query;
   if (page === undefined) page = 1;
   //page = parseInt(page);
-  if (nickname === undefined) nickname = '';
+  let propNickname = '';
+  if (nickname !== undefined) {
+    propNickname = nickname;
+  }
 
-  let { userList, curPage, numUser } = await getUserList(page, nickname);
-  console.log(typeof userList);
-  userList = [];
+  let { userList, curPage, numUser } = await getUserList(page, propNickname);
   return ({
     props: {
-      userList, curPage, numUser, nickname
+      userList, curPage, numUser, propNickname
     }
   })
 }
