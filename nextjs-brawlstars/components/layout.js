ae4eebb26f556ef3a7372db5f835d9a5d7de7ab4
JH-Ha <cubeprince@gmail.com>
2022-04-01 23:36:01 +0900

    [front] add user registration page

--- nextjs-brawlstars/components/layout.js
+++ nextjs-brawlstars/components/layout.js
@@ -1,12 +1,13 @@
 import Navbar from './navbar'
 import Footer from './footer'
+import styles from "../styles/Layout.module.scss";
 
 export default function Layout({ children }) {
     return (
-        <>
+        <div className={styles.layout}>
             <Navbar />
-            <main>{children}</main>
+            <main className={styles.main}>{children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
