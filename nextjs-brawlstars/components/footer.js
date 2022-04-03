96adee3dd4b68568630caf81e24c341a225e1012
JH-Ha <cubeprince@gmail.com>
2022-04-03 16:26:28 +0900

    [front] add links of youtube and twitter

--- nextjs-brawlstars/components/footer.js
+++ nextjs-brawlstars/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Twitter, Youtube } from 'react-bootstrap-icons';
 import styles from "../styles/Footer.module.scss";
 
 const Footer = () => {
@@ -7,21 +7,19 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.snsContainer}>
-                <div className={styles.icon}>
-                    <img src="/images/icon/twitter.svg" alt="twitter icon" />
-                </div>
-                <div className={styles.icon}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-youtube" viewBox="0 0 16 16">
-                        <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
-                    </svg>
-                </div>
+                <a className={styles.icon} href="https://www.youtube.com/channel/UCXmfZv4gZLbQfR8RKGOhXkw">
+                    <Youtube />
+                </a>
+                <a className={styles.icon} href="https://twitter.com/brawl_meta">
+                    <Twitter />
+                </a>
             </div>
             <div>
                 This material is unofficial and is not endorsed by Supercell.
                 <br />
                 For more information see <a href="https://www.supercell.com/fan-content-policy">Supercell&apos;s Fan Content Policy</a>
             </div>
-        </footer>
+        </footer >
     )
 };
 
