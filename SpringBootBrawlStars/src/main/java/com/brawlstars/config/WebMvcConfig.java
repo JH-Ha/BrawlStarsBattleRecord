141fdd5781a224449c6d59e86dd514f749d56790
JH-Ha <cubeprince@gmail.com>
2022-04-05 23:15:16 +0900

    [back] remove ** because not allowed

--- SpringBootBrawlStars/src/main/java/com/brawlstars/config/WebMvcConfig.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/config/WebMvcConfig.java
@@ -1,7 +1,7 @@
 package com.brawlstars.config;
 
+import com.brawlstars.interceptor.LoggerInterceptor;
 import java.io.IOException;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,13 +11,11 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-
-import com.brawlstars.interceptor.LoggerInterceptor;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("/**/*")
+      registry.addResourceHandler("/*")
         .addResourceLocations("classpath:/static/")
         .resourceChain(true)
         .addResolver(new PathResourceResolver() {
