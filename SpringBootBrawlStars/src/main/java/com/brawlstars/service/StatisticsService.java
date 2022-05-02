4abad64920198602c334561b1abf8e3e617e1143
JH-Ha <cubeprince@gmail.com>
2022-05-02 23:19:45 +0900

    [back] Use statistics cache to reduce data transfer
    Save statistics for 30 minutes

--- SpringBootBrawlStars/src/main/java/com/brawlstars/service/StatisticsService.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/service/StatisticsService.java
@@ -1,8 +1,12 @@
 package com.brawlstars.service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,23 +19,71 @@ import com.brawlstars.util.CommonUtil;
 @Transactional
 public class StatisticsService {
 
-	@Autowired
-	StatisticsRepository statisticsRepository;
+  @Autowired
+  StatisticsRepository statisticsRepository;
 
-	public List<RecordResultDto> getStats(String mode, String map, List<String> yearMonth) {
-		if(CommonUtil.isTrioMode(mode) || CommonUtil.isDuels(mode)) {
-			return getTrioStats(mode, map, yearMonth);
-		} else if(CommonUtil.isDuoShowdown(mode) || CommonUtil.isSolo(mode)) {
-			return getDuoSoloStats(mode, map, yearMonth);
-		}
-		return new ArrayList<>();
-	}
+  final int CACHE_TIME = 30; // MINUTES
 
-	public List<RecordResultDto> getTrioStats(String mode, String map, List<String> yearMonth) {
-		return statisticsRepository.getTrioStatByListYearMonth(mode, map, yearMonth);
-	}
+  static class CacheContainer {
 
-	public List<RecordResultDto> getDuoSoloStats(String mode, String map, List<String> yearMonth) {
-		return statisticsRepository.getDuoSoloStatByListYearMonth(mode, map, yearMonth);
-	}
+    LocalDateTime localDateTime;
+    List<RecordResultDto> recordResultDtos;
+
+    public LocalDateTime getLocalDateTime() {
+      return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+      this.localDateTime = localDateTime;
+    }
+
+    public List<RecordResultDto> getRecordResultDtos() {
+      return recordResultDtos;
+    }
+
+    public void setRecordResultDtos(
+        List<RecordResultDto> recordResultDtos) {
+      this.recordResultDtos = recordResultDtos;
+    }
+  }
+
+  private final Map<String, CacheContainer> cache = new HashMap<>();
+
+  public String makeKey(String mode, String map, List<String> yearMonth) {
+    return mode + map + String.join("", yearMonth);
+  }
+
+  public List<RecordResultDto> getStats(String mode, String map, List<String> yearMonth) {
+    if (CommonUtil.isTrioMode(mode) || CommonUtil.isDuels(mode)) {
+      return getTrioStats(mode, map, yearMonth);
+    } else if (CommonUtil.isDuoShowdown(mode) || CommonUtil.isSolo(mode)) {
+      return getDuoSoloStats(mode, map, yearMonth);
+    }
+    return new ArrayList<>();
+  }
+
+  public List<RecordResultDto> getStatsFromCache(String mode, String map, List<String> yearMonth) {
+    final String key = makeKey(mode, map, yearMonth);
+    CacheContainer cacheContainer = cache.get(key);
+    LocalDateTime now = LocalDateTime.now();
+    if (cacheContainer == null
+        || cacheContainer.getLocalDateTime().until(now, ChronoUnit.MINUTES) >= CACHE_TIME) {
+      List<RecordResultDto> resultDtos = getStats(mode, map, yearMonth);
+      CacheContainer newCacheContainer = new CacheContainer();
+      newCacheContainer.setLocalDateTime(now);
+      newCacheContainer.setRecordResultDtos(resultDtos);
+      cache.put(key, newCacheContainer);
+      return resultDtos;
+    } else {
+      return cacheContainer.getRecordResultDtos();
+    }
+  }
+
+  public List<RecordResultDto> getTrioStats(String mode, String map, List<String> yearMonth) {
+    return statisticsRepository.getTrioStatByListYearMonth(mode, map, yearMonth);
+  }
+
+  public List<RecordResultDto> getDuoSoloStats(String mode, String map, List<String> yearMonth) {
+    return statisticsRepository.getDuoSoloStatByListYearMonth(mode, map, yearMonth);
+  }
 }
