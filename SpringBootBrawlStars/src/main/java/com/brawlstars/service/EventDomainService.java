ad31fb5c164a9ac7c69a96473d02b413612ffee1
JH-Ha <cubeprince@gmail.com>
2022-05-06 23:32:59 +0900

    [back] Add event domain

---
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/service/EventDomainService.java
@@ -0,0 +1,35 @@
+package com.brawlstars.service;
+
+import com.brawlstars.domain.EventDomain;
+import com.brawlstars.json.EventInfo;
+import com.brawlstars.repository.EventDomainRepository;
+import java.util.Arrays;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class EventDomainService {
+
+  @Autowired
+  EventDomainRepository eventDomainRepository;
+
+  public EventDomain findById(String eventId) {
+    return eventDomainRepository.findByEventId(eventId);
+  }
+
+  public EventDomain save(EventInfo eventInfo) {
+    String eventId = eventInfo.getEvent().getId();
+    String mode = eventInfo.getEvent().getMode();
+    String map = eventInfo.getEvent().getMode();
+
+    EventDomain eventDomain = new EventDomain();
+    eventDomain.setEventId(eventId);
+    eventDomain.setMode(mode);
+    eventDomain.setMap(map);
+
+    return eventDomainRepository.save(eventDomain);
+  }
+
+}
