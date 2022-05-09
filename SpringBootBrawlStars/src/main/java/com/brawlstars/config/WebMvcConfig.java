f4d4ebbf3a2284a73a2457f152d38c7a9295b6b3
JH-Ha <cubeprince@gmail.com>
2022-05-10 01:52:10 +0900

    [back] remove logger interceptor because of latency

--- SpringBootBrawlStars/src/main/java/com/brawlstars/config/WebMvcConfig.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/config/WebMvcConfig.java
@@ -1,16 +1,12 @@
 package com.brawlstars.config;
 
 import com.brawlstars.interceptor.LoggerInterceptor;
-import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 //    @Override
@@ -29,24 +25,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //            }
 //        });
 //    }
-    
-    @Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loggerInterceptor()).excludePathPatterns("/images/**","/static/**");
-	}
-    
-    @Bean
-    public LoggerInterceptor loggerInterceptor() {
-    	return new LoggerInterceptor();
-    }
-    
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081", "http://127.0.0.1:8081", 
-                		"http://www.brawlstat.xyz", "http://www.brawlstat.xyz:8080",
-                		"https://www.brawlstat.xyz", "https://brawlstat.xyz",
-                		"https://brawlmeta.com", "https://www.brawlmeta.com");
-    }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    //registry.addInterceptor(loggerInterceptor()).excludePathPatterns("/images/**","/static/**");
+  }
+
+  @Bean
+  public LoggerInterceptor loggerInterceptor() {
+    return new LoggerInterceptor();
+  }
+
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:8081", "http://127.0.0.1:8081",
+            "http://www.brawlstat.xyz", "http://www.brawlstat.xyz:8080",
+            "https://www.brawlstat.xyz", "https://brawlstat.xyz",
+            "https://brawlmeta.com", "https://www.brawlmeta.com");
+  }
 }
