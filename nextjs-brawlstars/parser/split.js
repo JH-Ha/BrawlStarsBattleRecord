332a4d61560b5383bf981a501aba1398ffcf0143
JH-Ha <cubeprince@gmail.com>
2022-07-17 21:43:39 +0900

    update sitemaps

--- nextjs-brawlstars/parser/split.js
+++ nextjs-brawlstars/parser/split.js
@@ -15,6 +15,7 @@ bounty	Bull Pen
 bounty	Canal Grande
 bounty	Cube Force
 bounty	Deeper Danger
+bounty	Don't turn around
 bounty	Dry Season
 bounty	Electric Zone
 bounty	Excel
@@ -24,7 +25,9 @@ bounty	Land Ahoy
 bounty	Layer Cake
 bounty	Overgrown Canyon
 bounty	Purple Paradise
+bounty	Quick Skip
 bounty	Shooting Star
+bounty	Side by Side
 bounty	Snake Prairie
 brawlBall	Backyard Bowl
 brawlBall	Beach Ball
@@ -32,11 +35,13 @@ brawlBall	Binary Coding
 brawlBall	Center Field
 brawlBall	Center Stage
 brawlBall	Coarse Course
+brawlBall	Double Jeopardy
 brawlBall	Extra Bouncy
 brawlBall	Field Goal
 brawlBall	Firm Grip
 brawlBall	Hairdryer Treatment
 brawlBall	High Score
+brawlBall	Make it Bounce
 brawlBall	Off the Line
 brawlBall	Penalty Kick
 brawlBall	Pinball Dreams
@@ -61,6 +66,8 @@ duels	SHROUDING SERPENT
 duels	TIGER TRAP
 duels	WARRIOR'S WAY
 duoShowdown	Acid Lakes
+duoShowdown	Boxed In
+duoShowdown	Brawler Speedway
 duoShowdown	Canyon Rivers
 duoShowdown	Cavern Churn
 duoShowdown	Dark Fantasies
@@ -75,6 +82,7 @@ duoShowdown	Flying Fantasies
 duoShowdown	Grassy Gorge
 duoShowdown	Hot Maze
 duoShowdown	Island Invasion
+duoShowdown	Jump Park
 duoShowdown	Mystic Touch
 duoShowdown	Outrageous Outback
 duoShowdown	Rockwall Brawl
@@ -83,7 +91,9 @@ duoShowdown	Safety Center
 duoShowdown	Scorched Stone
 duoShowdown	Skull Creek
 duoShowdown	Stocky Stockades
+duoShowdown	Teleport Tag
 duoShowdown	The Mortuary
+duoShowdown	Zoomed Out
 gemGrab	Acute Angle
 gemGrab	Bouncing Diner
 gemGrab	Chill Space
@@ -93,12 +103,14 @@ gemGrab	Crystal Arcade
 gemGrab	Deathcap Trap
 gemGrab	Deep Diner
 gemGrab	Double Swoosh
+gemGrab	Duality
 gemGrab	Flooded Dam
 gemGrab	Flooded Mine
 gemGrab	Four Squared
 gemGrab	Gem Exchange
 gemGrab	Gem Fort
 gemGrab	Gem Source
+gemGrab	Glass Half Full
 gemGrab	Hard Rock Mine
 gemGrab	Minecart Madness
 gemGrab	Open Space
@@ -109,21 +121,26 @@ gemGrab	Spring Trap
 gemGrab	Undermine
 heist	Bridge Too Far
 heist	Can't touch this
+heist	Crossroads
 heist	Diagonal Alley
+heist	Fancy Fencing
 heist	Forks Out
 heist	G.G. Mortuary
 heist	Hot Potato
 heist	Kaboom Canyon
 heist	Pit Stop
 heist	Rattlesnake Ravine
+heist	River Banks
 heist	Rolling Rumble
 heist	Safe Zone
 heist	Tornado Ring
+heist	Treefecta
 heist	Twisted Gems
 hotZone	Controller Chaos
 hotZone	Danger Zone
 hotZone	Double Sided
 hotZone	Dueling Beetles
+hotZone	Imbalance
 hotZone	Mosh Pit
 hotZone	Night at the Museum
 hotZone	Open Business
@@ -132,11 +149,22 @@ hotZone	Parallel Plays
 hotZone	Ring of Fire
 hotZone	Split
 hotZone	Temple of Boom
+hotZone	Trident
+hunters	Deliverance
+hunters	Hunting Season
+hunters	Omega
+hunters	Quick and Restless
+hunters	The Prey
+hunters	Trials and Tribulations
+hunters	True Trail
 invasion	A Loud Place
 invasion	Bots Attack!
 invasion	Brawl of the Worlds
+invasion	Departure
 invasion	Devolution
+invasion	Hunting Season
 invasion	The Bot
+invasion	Trials and Tribulations
 invasion	Val Verde
 knockout	Belle's Rock
 knockout	Crimewater
@@ -145,9 +173,11 @@ knockout	Ends Meet
 knockout	Flaring Phoenix
 knockout	Flowing Springs
 knockout	Goldarm Gulch
+knockout	Hidden Enemy
 knockout	Luis' Revenge
 knockout	Middle Ground
 knockout	New Perspective
+knockout	Out in the Open
 knockout	Riverside
 knockout	Splash Out
 knockout	X Marks the Spot
@@ -170,6 +200,7 @@ siege	Robo Highway
 siege	Robo Ring
 siege	Some Assembly Required
 soloShowdown	Acid Lakes
+soloShowdown	Boxed In
 soloShowdown	Canyon Rivers
 soloShowdown	Cavern Churn
 soloShowdown	Dark Fantasies
@@ -193,6 +224,7 @@ soloShowdown	Scorched Stone
 soloShowdown	Skull Creek
 soloShowdown	Stocky Stockades
 soloShowdown	The Mortuary
+soloShowdown	Zoomed Out
 wipeout	Camping Grounds
 wipeout	Friendly Fire: Off
 wipeout	Infinite Doom
@@ -217,11 +249,11 @@ data.split('\n').map(a => {
 }).map(b => {
     return `/map/${encodeURIComponent(b.map)}/mode/${b.mode}`;
 }).forEach(c => {
-    result += `<url><loc>https://brawlmeta.com/ko${c}</loc><lastmod>2022-05-19T09:30:00+09:00</lastmod></url>
+    result += `<url><loc>https://brawlmeta.com/ko${c}</loc><lastmod>2022-07-16T09:30:00+09:00</lastmod></url>
     `
-    result += `<url><loc>https://brawlmeta.com/ja${c}</loc><lastmod>2022-05-19T09:30:00+09:00</lastmod></url>
+    result += `<url><loc>https://brawlmeta.com/ja${c}</loc><lastmod>2022-07-16T09:30:00+09:00</lastmod></url>
     `
-    result += `<url><loc>https://brawlmeta.com/en${c}</loc><lastmod>2022-05-19T09:30:00+09:00</lastmod></url>
+    result += `<url><loc>https://brawlmeta.com/en${c}</loc><lastmod>2022-07-16T09:30:00+09:00</lastmod></url>
     `
 })
 
