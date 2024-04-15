15708b4826466b90f48a15f434a5b4e156dfb808
JH-Ha <cubeprince@gmail.com>
2024-04-16 01:10:46 +0900

    [back] Add test data

---
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/test/TestDataService.java
@@ -0,0 +1,28 @@
+package com.brawlstars.test;
+
+import com.brawlstars.api.BrawlStarsAPI;
+import com.brawlstars.schedule.RecordSchedule;
+import com.brawlstars.service.MemberService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("local")
+public class TestDataService {
+
+  @Autowired
+  private BrawlStarsAPI brawlStarsAPI;
+  @Autowired
+  private MemberService memberService;
+  @Autowired
+  private RecordSchedule recordSchedule;
+
+  @PostConstruct
+  public void postConstruct() {
+    String tag = "#9QU209UYC";
+    memberService.saveMember(tag);
+    recordSchedule.saveRecords();
+  }
+}
