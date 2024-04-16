bb5199cfad66b8999e9245fa1bf5f8d01eb33e34
JH-Ha <cubeprince@gmail.com>
2024-04-17 00:00:24 +0900

    [back] Add index to (record_id,group_key) for searching battle records

--- SpringBootBrawlStars/src/main/java/com/brawlstars/domain/Record.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/domain/Record.java
@@ -29,77 +29,79 @@ import org.hibernate.annotations.OnDeleteAction;
 @DiscriminatorColumn(name = "game_type")
 @Getter
 @Setter
-@Table(uniqueConstraints = { @UniqueConstraint(name = "uniqueGame", columnNames = { "tag", "battleTime", "brawlerName" }) },
-		indexes = {
-			@Index(name = "gameStatIdx", columnList = "mode, map, statUpdated"),
-			@Index(name = "gameTrendIdx", columnList = "recordDate, mode, map, brawlerName")
-		})
+@Table(uniqueConstraints = {
+    @UniqueConstraint(name = "uniqueGame", columnNames = {"tag", "battleTime", "brawlerName"})},
+    indexes = {
+        @Index(name = "gameStatIdx", columnList = "mode, map, statUpdated"),
+        @Index(name = "gameTrendIdx", columnList = "recordDate, mode, map, brawlerName"),
+        @Index(name = "recordJoinIdx", columnList = "record_id, group_key")
+    })
 public class Record {
 
-	@Id
-	@GeneratedValue
-	@Column(name = "record_id")
-	private Long id;
-	
-	@Column(length = 8)
-	private String recordDate;
-	@Column(length = 20)
-	private String tag;
-	@Column(length = 25)
-	private String battleTime;
-	@Column(length = 25)
-	private String brawlerName;
-	@Column(length = 25)
-	private String brawlerId;
-	private Integer power;
-	private Integer trophies;
-	private Integer trophyChange;
-	
-	@Column(length = 50)
-	private String map;
-	
-	@Column(length = 25)
-	private String eventId;
-	
-	@Column(length = 100)
-	private String playerName;
+  @Id
+  @GeneratedValue
+  @Column(name = "record_id")
+  private Long id;
 
-	// @Column(name = "group_key", insertable = false, updatable = false)
-	// private String groupKey;
-	@Column(length = 50)
-	private String mode;
-	
-	@Column(length = 25)
-	private String type;
+  @Column(length = 8)
+  private String recordDate;
+  @Column(length = 20)
+  private String tag;
+  @Column(length = 25)
+  private String battleTime;
+  @Column(length = 25)
+  private String brawlerName;
+  @Column(length = 25)
+  private String brawlerId;
+  private Integer power;
+  private Integer trophies;
+  private Integer trophyChange;
 
-	//Trio
-	private Integer duration;
-	private Boolean isStarPlayer;
-	@Column(length = 50)
-	private String result;
-	
-	private Integer teamId;
-	//duo, solo
-	private Integer resultRank;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@OnDelete(action= OnDeleteAction.CASCADE)
-	@JoinColumn(name = "group_key", foreignKey = @ForeignKey(name = "none"))
-	private Record parent;
+  @Column(length = 50)
+  private String map;
 
-	@OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
-	private List<Record> groupRecords = new ArrayList<>();
+  @Column(length = 25)
+  private String eventId;
 
-	@ColumnDefault("false")
-	private Boolean statUpdated;
-	
-	@ColumnDefault("false")
-	private Boolean isStatCandidate;
-	
-	public static void setRelation(Record parent, List<Record> groupRecords) {
-		parent.setGroupRecords(groupRecords);
-		groupRecords.stream().forEach(gr -> {
-			gr.setParent(parent);
-		});
-	}
+  @Column(length = 100)
+  private String playerName;
+
+  // @Column(name = "group_key", insertable = false, updatable = false)
+  // private String groupKey;
+  @Column(length = 50)
+  private String mode;
+
+  @Column(length = 25)
+  private String type;
+
+  //Trio
+  private Integer duration;
+  private Boolean isStarPlayer;
+  @Column(length = 50)
+  private String result;
+
+  private Integer teamId;
+  //duo, solo
+  private Integer resultRank;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @OnDelete(action = OnDeleteAction.CASCADE)
+  @JoinColumn(name = "group_key", foreignKey = @ForeignKey(name = "none"))
+  private Record parent;
+
+  @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+  private List<Record> groupRecords = new ArrayList<>();
+
+  @ColumnDefault("false")
+  private Boolean statUpdated;
+
+  @ColumnDefault("false")
+  private Boolean isStatCandidate;
+
+  public static void setRelation(Record parent, List<Record> groupRecords) {
+    parent.setGroupRecords(groupRecords);
+    groupRecords.stream().forEach(gr -> {
+      gr.setParent(parent);
+    });
+  }
 }
