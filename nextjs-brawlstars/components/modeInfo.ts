0f94217de69d32db3dc31787e6489913eb577fa2
JH-Ha <cubeprince@gmail.com>
2024-07-14 19:35:54 +0900

    [front] Fix a bug where hotZone was duplicated

--- nextjs-brawlstars/components/modeInfo.ts
+++ nextjs-brawlstars/components/modeInfo.ts
@@ -1,58 +1,55 @@
 type mode = {
-    "name": string
-}
-const modeInfo:mode[] = [
-    {
-        "name": "gemGrab",
-    },
-    {
-        "name": "heist",
-    },
-    {
-        "name": "brawlBall",
-    },
-    {
-        "name": "bounty",
-    },
-    {
-        "name": "siege",
-    },
-    {
-        "name": "hotZone",
-    },
-    {
-        "name": "knockout",
-    },
-    {
-        "name": "hotZone",
-    },
-    {
-        "name": "duels"
-    },
-    {
-        "name": "wipeout",
-    },
-    {
-        "name": "payload",
-    },
-    {
-        "name": "basketBrawl",
-    },
-    {
-        "name": "hunters",
-    },
-    {
-        "name": "soloShowdown",
-    },
-    {
-        "name": "duoShowdown",
-    },
-    {
-        "name": "volleyBrawl",
-    },
-    {
-        "name": "holdTheTrophy",
-    }
+  name: string;
+};
+const modeInfo: mode[] = [
+  {
+    name: "gemGrab",
+  },
+  {
+    name: "heist",
+  },
+  {
+    name: "brawlBall",
+  },
+  {
+    name: "bounty",
+  },
+  {
+    name: "siege",
+  },
+  {
+    name: "knockout",
+  },
+  {
+    name: "hotZone",
+  },
+  {
+    name: "duels",
+  },
+  {
+    name: "wipeout",
+  },
+  {
+    name: "payload",
+  },
+  {
+    name: "basketBrawl",
+  },
+  {
+    name: "hunters",
+  },
+  {
+    name: "soloShowdown",
+  },
+  {
+    name: "duoShowdown",
+  },
+  {
+    name: "volleyBrawl",
+  },
+  {
+    name: "holdTheTrophy",
+  },
 ];
 
 export { modeInfo };
