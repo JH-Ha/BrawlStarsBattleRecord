0fb873288c828f05222f9badc28acc1796febd1f
JH-Ha <cubeprince@gmail.com>
2024-07-20 21:08:18 +0900

    [back] Refactoring MemberServiceTest for better code readability

--- SpringBootBrawlStars/src/test/java/com/brawlstars/MemberServiceTest.java
+++ SpringBootBrawlStars/src/test/java/com/brawlstars/MemberServiceTest.java
@@ -3,7 +3,6 @@ package com.brawlstars;
 import com.brawlstars.json.BattleLog;
 import com.brawlstars.json.Item;
 import com.brawlstars.repository.MemberDto;
-import com.brawlstars.repository.MemberRepository;
 import com.brawlstars.service.MemberService;
 import com.brawlstars.service.RecordService;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -13,7 +12,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +19,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-public class MemberTest {
+public class MemberServiceTest {
 	@Autowired
 	MemberService memberService;
-	@Autowired
-	MemberRepository memberRepository;
 	@Autowired
 	RecordService recordService;
 	
@@ -47,18 +45,28 @@ public class MemberTest {
 	}
 	
 	@Test
-	public void findMemberByTag() {
+	public void testGetMemberByTag() {
+		// Given
 		String tag = "#9QU209UYC";
 		String name = "폼폼";
-		
-		MemberDto foundMember = memberRepository.findMemberByTag(tag);
-		Assertions.assertThat(foundMember.getName()).isEqualTo(name);
+
+		// When
+		MemberDto foundMember = memberService.getMemberByTag(tag);
+
+		// Then
+		assertThat(foundMember.getName()).isEqualTo(name);
 	}
 	
 	@Test
-	public void findAll() {
-		Page<MemberDto> members = memberRepository.findAll("", PageRequest.of(0, 10));
-		Assertions.assertThat(members.getSize()).isEqualTo( 10);
+	public void testGetMembers() {
+		// Given
+		PageRequest pageRequest = PageRequest.of(0, 10);
+
+		// When
+		Page<MemberDto> members = memberService.getMembers("", pageRequest);
+
+		// Then
+		assertThat(members.getSize()).isEqualTo( 10);
 	}
 	
 }
