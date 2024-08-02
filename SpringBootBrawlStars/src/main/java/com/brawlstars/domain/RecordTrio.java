1991b9bcf721516311355d99fc0143b6d8055451
JH-Ha <cubeprince@gmail.com>
2024-08-02 09:32:56 +0900

    [back] Fix a bug where paint brawl was saved as gemGrab mode

--- SpringBootBrawlStars/src/main/java/com/brawlstars/domain/RecordTrio.java
+++ SpringBootBrawlStars/src/main/java/com/brawlstars/domain/RecordTrio.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Optional;
+
 @Entity
 @DiscriminatorValue("TRIO")
 @Getter
@@ -17,7 +19,10 @@ public class RecordTrio extends Record {
     public static RecordTrio createRecord(Item item, String tag, Player player, int playerGroupIdx, int i) {
         RecordTrio recordTrio = new RecordTrio();
         String map = item.getEvent().getMap();
-        String mode = item.getBattle().getMode();
+        // event.mode and battle.mode are different when mode is paintBrawl.
+        // The event.mode is paintBrawl, but battle.mode is gemGrab
+        String mode = Optional.ofNullable(item.getEvent().getMode())
+                .orElse(item.getBattle().getMode());
         String brawlerName = player.getBrawler().getName().replace("\n", " ");
         Integer trophies = player.getBrawler().getTrophies();
 
